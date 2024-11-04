64c84697265ed8d1f5e2d31e37b6f23e47f6c48e
Ricardo <RicardoCzj@foxmail.com>
2024-11-04 15:41:32 +0800

    feat: add

--- others/random57.js
+++ others/random57.js
@@ -4,6 +4,31 @@ function random5() {
   return Math.floor(Math.random() * 5) + 1; // Math.random() * 5 生成 0-4 的随机数，+1 生成 1-5 的随机数
 }
 
+// 2-5的随机数，不使用 random5实现
+function random2_5() {
+  // 不使用 random5实现
+  return Math.floor(Math.random() * 4) + 2; // 0-3 + 2 = 2-5
+}
+
+// 2-10的随机数，不使用 random5实现
+function random2_10() {
+  // 不使用 random5实现
+  return Math.floor(Math.random() * 9) + 2; // 0-8 + 2 = 2-10
+  // 3-10
+  // return Math.floor(Math.random() * 8) + 3; // 0-7 + 3 = 3-10
+}
+
+// function random2_5() {
+//   // let num = random5();
+//   // while (num === 5) {
+//   //   num = random5();
+//   // }
+//   // return num % 2 + 2; // 1-4
+
+
+// }
+
+
 // 1. 生成 1-25 的随机数
 // 2. 如果随机数大于 21，则重新生成
 // 3. 返回随机数除以 7 的余数加 1
---
+++ others/数字相加.js
@@ -0,0 +1,21 @@
+// 数字（字符串）模拟相加
+
+function add(a, b) {
+  a = a.toString();
+  b = b.toString();
+  let res = '';
+  let carry = 0;
+  let i = a.length - 1;
+  let j = b.length - 1;
+  while (i >= 0 || j >= 0) {
+    const sum = (+a[i] || 0) + (+b[j] || 0) + carry;
+    res = (sum % 10) + res;
+    carry = sum >= 10 ? 1 : 0;
+    i--;
+    j--;
+  }
+  return carry ? '1' + res : res;
+}
+
+// test
+console.log(add(1, 2)); // '3'
